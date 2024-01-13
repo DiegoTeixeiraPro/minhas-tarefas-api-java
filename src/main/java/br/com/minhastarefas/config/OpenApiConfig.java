a271146b7596013ef890cfe18c8353e7223ffc30
Diego Teixeira <diegoateixeira.pro@gmail.com>
2024-01-13 18:50:15 -0300

    Atualizacao do Projeto

--- src/main/java/br/com/minhastarefas/config/OpenApiConfig.java
+++ src/main/java/br/com/minhastarefas/config/OpenApiConfig.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Configuration;
 public class OpenApiConfig {
 
     @Bean
-    public OpenAPI customOpenAPI() {
+    OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
                         .title("Minhas Tarefas")
