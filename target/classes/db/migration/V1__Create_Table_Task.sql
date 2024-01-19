ec012c877b5622a26716074cf2d9456e393dab52
Diego Teixeira <diegoateixeira.pro@gmail.com>
2024-01-19 15:34:26 -0300

    Atualizacao

--- target/classes/db/migration/V1__Create_Table_Task.sql
+++ target/classes/db/migration/V1__Create_Table_Task.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `tb_tasks`
     `id_product`  binary(16)   NOT NULL,
     `title`       varchar(40)  NOT NULL,
     `description` varchar(120) NOT NULL,
-    `status`      tinyint(1)   NOT NULL,
+    `status`      BOOLEAN      NOT NULL,
     PRIMARY KEY (`id_product`)
 );
